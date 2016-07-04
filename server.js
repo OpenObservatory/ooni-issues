6b0557e762aec1e87def74a7a88717db65858e32
Will Scott <willscott@gmail.com>
2016-07-04 13:01:00 -0700

    sketch out auth and submission of github issue

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ var path = require('path'),
     express = require('express'),
     bodyParser = require('body-parser'),
     csrf = require('csurf'),
+    fs = require('fs'),
+    githubApi = require('github'),
     csrfProtection = csrf({ cookie: false }),
     parseForm = bodyParser.urlencoded({ extended: false }),
     app = express(),
     port = process.env.PORT || 3000,
-    router = express.Router();
+    router = express.Router(),
+    config = JSON.parse(fs.readFileSync('config.json')),
+    github = new githubApi({});
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "views")));
@@ -14,8 +18,20 @@ app.use(express.static(path.join(__dirname, "views")));
 router.get('/', function(req, res, next) {
   res.render('index.html', { csrfToken: req.csrfToken()});
 });
+
+
 router.post('/submit', parseForm, csrfProtection, function (req, res) {
-  //TODO: submit issue.
+  github.authenticate({
+    type: "oauth",
+    token: config.token
+  });
+  github.issues.create({
+    user: config.user,
+    repo: config.repo,
+    title: "Test Issue",
+    body: "Issue Body",
+    labels: ["Autosubmitted"]
+  });
 });
 
 app.use('/', router);
